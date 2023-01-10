a9fdfb2f590c72dd95bd9bd758392733244a2ded
iAmOutlined <122179009+iAmOutlined@users.noreply.github.com>
2023-01-10 05:40:27 -0600

    Create AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: CompilationRelaxations(8)]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.EnableEditAndContinue)]
+[assembly: AssemblyTitle("AstralFN")]
+[assembly: AssemblyDescription("Welcome to Splasg, A Fortite Private Server, Hybrid Server.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Splash")]
+[assembly: AssemblyProduct("Splasg")]
+[assembly: AssemblyCopyright("Copyright © Outlined 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("bcb7ccff-1fd7-406f-a696-3be6aa89d31c")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
